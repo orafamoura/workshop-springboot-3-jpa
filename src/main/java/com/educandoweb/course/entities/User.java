ab9506f9715df73c5cdb109909a3641b7226fe8a
Raphael moura <raphaelmvb2@gmail.com>
2023-10-01 21:03:30 -0300

    Order, Instant, ISO 8601 pt.1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,20 +1,24 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{ // serializable voce define ela quando voce quer que os objetos possam ser transformados em cadeias de bites, para trafegar na rede, possa ser gravado em arquivos etc.
+public class User implements Serializable { // serializable voce define ela quando voce quer que os objetos possam ser
+											// transformados em cadeias de bites, para trafegar na rede, possa ser
+											// gravado em arquivos etc.
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,7 +26,10 @@ public class User implements Serializable{ // serializable voce define ela quand
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>(); // no caso de uma colection so se coloca o get nao o set
+
 	public User() { // por usar framework obrigatoriamente tem que usar um constructor vazio
 	}
 
@@ -75,6 +82,10 @@ public class User implements Serializable{ // serializable voce define ela quand
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,11 +102,5 @@ public class User implements Serializable{ // serializable voce define ela quand
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
